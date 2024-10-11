b535dc0865ce70ff8a2d692f9c1e971398f10c6f
Walaakhkh <Walaakhalil98@gmail.com>
2024-10-11 02:35:34 +0000

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
+"""
+0-lockboxes.py
+
+This module contains a method that determines if all boxes can be opened.
+"""
+
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened."""
-    # Start with the first box (box 0) already unlocked
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list where each element is a list representing
+                               a box and contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, otherwise False.
+    """
+    # List to keep track of unlocked boxes, initially only box 0 is unlocked
     unlocked = [False] * len(boxes)
     unlocked[0] = True
     
-    # Create a stack to hold keys, starting with the keys in box 0
+    # Stack to manage the keys we find, starting with the keys in box 0
     keys = [0]
     
-    # Process the keys one by one
+    # While there are keys to process
     while keys:
-        current_key = keys.pop()
+        current_key = keys.pop()  # Take a key from the stack
         for key in boxes[current_key]:
-            # If the key opens a new box, unlock it and collect the keys inside
+            # If the key opens a new box and it's a valid box number
             if key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
+                unlocked[key] = True  # Mark the box as unlocked
+                keys.append(key)      # Add the new key to the stack
     
-    # If all boxes are unlocked, return True, otherwise return False
+    # Check if all boxes have been unlocked
     return all(unlocked)
